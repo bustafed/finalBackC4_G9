49159b919342ee10233493219bf4a94e699828f4
federico bustamante <fede.dhouse@gmail.com>
2023-09-17 16:22:39 -0300

    added methods for dentists

--- cmd/external/database/service.go
+++ cmd/external/database/service.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/bustafed/finalBackC4_G9/internal/dentists"
 	"github.com/bustafed/finalBackC4_G9/internal/patients"
+	"log"
 )
 
 type SqlStore struct {
@@ -40,20 +41,49 @@ func (s *SqlStore) GetDentistByID(id int) (dentists.Dentist, error) {
 	return dentistReturn, nil
 }
 
-/*
-func (s *SqlStore) Modify(id int, product products.Product) (products.Product, error) {
-	query := fmt.Sprintf("UPDATE products SET name = '%s', quantity = %v, code_value = '%s',"+
-		" is_published = %v, expiration = '%s', price = %v WHERE id = %v;", product.Name, product.Quantity,
-		product.CodeValue, product.IsPublished, product.Expiration, product.Price, product.ID)
-	stmt, err := s.DB.Prepare(query)
+func (s *SqlStore) CreateDentist(d dentists.Dentist) (dentists.Dentist, error) {
+	stmt, err := s.DB.Prepare("INSERT INTO dentists (name, surname, license) VALUES ($1, $2, $3) RETURNING id")
 	if err != nil {
-		return products.Product{}, err
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	var insertedId int
+	err = stmt.QueryRow(d.Name, d.Surname, d.License).Scan(&insertedId)
+
+	if err != nil {
+		return dentists.Dentist{}, err
 	}
 
-	_, err = stmt.Exec()
-	if err != nil {
-		return products.Product{}, err
-	}
+	d.ID = insertedId
+	return d, nil
+}
 
-	return product, nil
-}*/
+func (s *SqlStore) UpdateDentistByID(id int, d dentists.Dentist) (dentists.Dentist, error) {
+	stmt, err := s.DB.Prepare("UPDATE dentists SET name = $1, surname = $2, license = $3 WHERE id = $4")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Query(d.Name, d.Surname, d.License, id)
+	if err != nil {
+		return dentists.Dentist{}, err
+	}
+	d.ID = id
+	return d, nil
+}
+
+func (s *SqlStore) DeleteDentistByID(id int) error {
+	stmt, err := s.DB.Prepare("DELETE FROM dentists WHERE id = $1")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer stmt.Close()
+
+	_, err = stmt.Query(id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
--- internal/dentists/service.go
+++ internal/dentists/service.go
@@ -2,15 +2,17 @@ package dentists
 
 type Repository interface {
 	GetDentistByID(id int) (Dentist, error)
+	CreateDentist(d Dentist) (Dentist, error)
+	UpdateDentistByID(id int, d Dentist) (Dentist, error)
+	DeleteDentistByID(id int) error
 }
 
 type Service struct {
 	repository Repository
 }
 
-func (s *Service) ModifyByID(id int, dentist Dentist) (Dentist, error) {
-	//TODO implement me
-	panic("implement me")
+func (s *Service) UpdateDentistByID(id int, dentist Dentist) (Dentist, error) {
+	return s.repository.UpdateDentistByID(id, dentist)
 }
 
 func NewService(repository Repository) *Service {
@@ -19,4 +21,12 @@ func NewService(repository Repository) *Service {
 
 func (s *Service) GetDentistByID(id int) (Dentist, error) {
 	return s.repository.GetDentistByID(id)
+}
+
+func (s *Service) CreateDentist(d Dentist) (Dentist, error) {
+	return s.repository.CreateDentist(d)
+}
+
+func (s *Service) DeleteDentistByID(id int) error {
+	return s.repository.DeleteDentistByID(id)
 }
